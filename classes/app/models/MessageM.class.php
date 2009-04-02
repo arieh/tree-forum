ca56017297d6fda0471ae1a5259b73364747b2b2
arieh.glazer <arieh.glazer@e0e5a594-1eb5-11de-82b5-194b17410922>
2009-04-02 15:52:38 +0000

    open message now works * for now uses LIKE statement. should be fixed

--- classes/app/models/MessageM.class.php
+++ classes/app/models/MessageM.class.php
@@ -4,11 +4,20 @@
  *  + 'add' : add a new message:
  * 		required:
  * 			- 'forum'(string)    : forum id
- * 			- 'title' (string)   : message title
- * 			- 'message' (string) : message content
+ * 			- 'title' (string)   : message title - must be longer than 2 chars
+ * 			- 'message' (string) : message content - must be longer than 2 chars
  * 			- 'parent' (int)     : the parent message. only required if not a base message
  * 		optional:
  * 			- 'base'(bool) : whether this is a base message or not (defualt: true)
+ * 		errors:
+ * 			- 'shortTitle' : title is too short or invalid
+ * 			- 'shortContent' : content was too short or invalid
+ *  + 'open' : open a message, and its chieldren's tree
+ * 		required:
+ * 			- 'id'(int) : message id
+ * 		optional:
+ * 			- 'children' : retrieve message children? default: true
+ * 		
  */ 
 class MessageM extends Model{
 	/**
@@ -19,7 +28,7 @@ class MessageM extends Model{
 	/**
 	 * @see <Model.class.php>
 	 */
-	protected $_actions = array('view','edit','add','delete');
+	protected $_actions = array('open','edit','add','delete');
 	
 	/**
 	 * @param int message id
@@ -34,6 +43,16 @@ class MessageM extends Model{
 	protected $_messages = array();
 	
 	/**
+	 * @param string message title
+	 */
+	private $_title = '';
+	
+	/**
+	 * @param string message content
+	 */
+	private $_content = '';
+	
+	/**
 	 * @see <Model.class.php>
 	 */
 	public function execute(){
@@ -41,10 +60,12 @@ class MessageM extends Model{
 			$this->setError('noPermision');
 			return false;
 		}
-		
 		switch ($this->getAction()){
 			case 'add':
 				$this->addMessage();
+			break;
+			case 'open':
+				$this->openMessage();
 			break;
 		}
 	}
@@ -75,16 +96,16 @@ class MessageM extends Model{
 			$title = $this->getOption('title');
 			$message = $this->getOption('message');
 			
-			if (!$title || !is_string($title) || strlen($title)<3) $this->setError('shortTitle');
-			if (!$message || !is_string($message) || strlen($message)<2) $this->setError('shortMessage');
+			if (!$title || !is_string($title) || strlen($title)<2) $this->setError('shortTitle');
+			if (!$message || !is_string($message) || strlen($message)<2) $this->setError('shortContent');
 			
 		if ($this->isError()) return false; 
 		
 		/*
 		 * post message
 		 */
-		if ($base) $this->postBaseMessage($forum,$title,$message,false);
-		else $this->postSubMessage($forum,$parent,$title,$message,false);
+		if ($base) $this->postBaseMessage($forum,$title,$message,true);
+		else $this->postSubMessage($forum,$parent,$title,$message,true);
 	}
 	
 	/**
@@ -106,8 +127,9 @@ class MessageM extends Model{
 	 * @access private
 	 * @return bool
 	 */
-	private function doesMessageExists($id,$forum,$log=false){
-		return ($this->_link->countFields('messages',array('forum_id'=>$forum,'id'=>$id),$log)>0);
+	private function doesMessageExists($id,$forum=false,$log=false){
+		if ($forum) return ($this->_link->countFields('messages',array('forum_id'=>$forum,'id'=>$id),$log)>0);
+		return ($this->_link->countFields('messages',array('id'=>$id),$log)>0);
 	}
 	
 	/**
@@ -155,7 +177,7 @@ class MessageM extends Model{
 		
 		$this->_link->update('messages',array('dna'=>$dna,'root_id'=>$root),array('id'=>$id),$log);
 		
-		$this->_link->insert('message_contents',array('id'=>$id,'title'=>$title,'message'=>$message),$log); 
+		$this->_link->insert('message_contents',array('message_id'=>$id,'title'=>$title,'message'=>$message,'non-html'=>strip_tags($message)),$log); 
 	}
 	
 	/**
@@ -168,6 +190,83 @@ class MessageM extends Model{
 		return $this->_link->select('messages',array('forum_id','root_id','dna','id'),array('id'=>$id),true,$log);
 	}
 	
+	/**
+	 * opens a message
+	 * @access private
+	 */
+	private function openMessage(){
+		$id = $this->getOption('id');
+		if (!$id || !is_numeric($id)) throw new MessageMException('badId');
+		
+		if ($this->doesMessageExists($id)==false) throw new MessageMException('badId');
+		
+		$msg = $this->retrieveMessageInfo($id,false);
+		$this->_id = $id;
+		$this->_title = $msg['title'];
+		$this->_content = $msg['message'];
+		
+		if ($this->isOptionSet('children') && $this->getOption('children') == false) return;
+		
+		$this->retrieveMessages($msg['dna'],false);
+		$this->orderMessages();
+	}
+	
+	
+	private function retrieveMessageInfo($id,$log=false){
+		$query = NewDao::getGenerator();
+		
+		$query->addSelect('messages',array());
+		$query->addSelect('message_contents',array('title','message'));
+		
+		$query->addInnerJoin(array('messages'=>'id'),array('message_contents'=>'message_id'));
+		
+		$query->addConditionSet(
+			$query->createCondition('messages','id','=',$id)
+		);
+		
+		$res = $this->_link->queryArray($query->generate(),$log);
+		return $res[0];
+	}
+	/**
+	 * retrieves the messages
+	 * 	@param string $dna the message's parent list
+	 * 	@param bool   $log log queries?
+	 * @access private
+	 */
+	private function retrieveMessages($dna,$log=false){
+		$query = NewDao::getGenerator();
+		
+		$query->addSelect('messages',array());
+		$query->addSelect('message_contents',array('title','message'));
+		
+		$query->addInnerJoin(array('messages'=>'id'),array('message_contents'=>'message_id'));
+		
+		$query->addConditionSet(
+			//this needs to be revised. should find a way to do this without a LIKE statement
+			$query->createCondition('messages','dna','LIKE',$dna.".%")
+		);
+		
+		$messages = $this->_link->queryArray($query->generate(),$log);
+		foreach ($messages as $msg) $this->_messages[$msg['dna']] = $msg;
+	}
+	
+	/**
+     * orders the messages retrieved from the database
+     * @access private
+     */
+    private function orderMessages(){
+    	if (count($this->_messages)==0) return;
+    	
+    	$keys = array_keys($this->_messages);
+    	natsort($keys);
+    	$messages = $this->_messages;
+    	$this->_messages = array();
+    	foreach ($keys as $key){
+    		$messages[$key]['depth'] = count(explode('.',$messages[$key]['dna']));
+    		$this->_messages[] = $messages[$key];
+    	}
+    	$this->_messages = array_reverse($this->_messages);
+    }
 }
 
 class MessageMException extends ModelException{}
