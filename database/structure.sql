58879475d54ec844a972d30ad0ad7cc3363fc33c
arieh.glazer <arieh.glazer@e0e5a594-1eb5-11de-82b5-194b17410922>
2009-04-02 11:17:57 +0000

    basic db structure and some sample data

--- database/structure.sql
+++ database/structure.sql
@@ -4,7 +4,7 @@ Source Host: 192.168.2.104
 Source Database: tree-forum
 Target Host: 192.168.2.104
 Target Database: tree-forum
-Date: 01/04/2009 17:52:06
+Date: 02/04/2009 14:17:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -17,7 +17,7 @@ CREATE TABLE `forums` (
   `description` varchar(255) NOT NULL,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for forums_permisions
@@ -37,7 +37,9 @@ DROP TABLE IF EXISTS `message_contents`;
 CREATE TABLE `message_contents` (
   `title` varchar(255) default NULL,
   `message` text,
-  `non-html` text
+  `non-html` text,
+  `message_id` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`message_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -51,7 +53,7 @@ CREATE TABLE `messages` (
   `root_id` int(11) default NULL,
   `forum_id` int(11) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Table structure for permisions
@@ -67,9 +69,17 @@ CREATE TABLE `permisions` (
 -- Records 
 -- ----------------------------
 INSERT INTO `forums` VALUES ('1', 'test forum', 'testing');
+INSERT INTO `forums` VALUES ('3', 'forum creation test', 'another test');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '9');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '10');
 INSERT INTO `messages` VALUES ('1', '1', '1', '1', '1');
 INSERT INTO `messages` VALUES ('2', '1.2', '0', '1', '1');
 INSERT INTO `messages` VALUES ('3', '3', '1', '3', '1');
 INSERT INTO `messages` VALUES ('4', '1.4', '0', '1', '1');
 INSERT INTO `messages` VALUES ('5', '3.5', '0', '3', '1');
 INSERT INTO `messages` VALUES ('6', '1.2.6', '0', '1', '1');
+INSERT INTO `messages` VALUES ('7', '7', '1', '7', '1');
+INSERT INTO `messages` VALUES ('8', '8', '1', '8', '1');
+INSERT INTO `messages` VALUES ('9', '9', '1', '9', '1');
+INSERT INTO `messages` VALUES ('10', '10', '1', '10', '3');
+INSERT INTO `messages` VALUES ('12', '3.5.12', '0', '3', '1');
