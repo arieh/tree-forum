26c99a640faa607fe2dab68c5044692cfbebdbff
arieh.glazer <arieh.glazer@e0e5a594-1eb5-11de-82b5-194b17410922>
2009-04-02 15:53:39 +0000

    better data (previous would not work- messages had no content)

--- database/structure.sql
+++ database/structure.sql
@@ -4,7 +4,7 @@ Source Host: 192.168.2.104
 Source Database: tree-forum
 Target Host: 192.168.2.104
 Target Database: tree-forum
-Date: 02/04/2009 14:17:28
+Date: 02/04/2009 18:53:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -53,7 +53,7 @@ CREATE TABLE `messages` (
   `root_id` int(11) default NULL,
   `forum_id` int(11) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Table structure for permisions
@@ -72,14 +72,19 @@ INSERT INTO `forums` VALUES ('1', 'test forum', 'testing');
 INSERT INTO `forums` VALUES ('3', 'forum creation test', 'another test');
 INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '9');
 INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '10');
-INSERT INTO `messages` VALUES ('1', '1', '1', '1', '1');
-INSERT INTO `messages` VALUES ('2', '1.2', '0', '1', '1');
-INSERT INTO `messages` VALUES ('3', '3', '1', '3', '1');
-INSERT INTO `messages` VALUES ('4', '1.4', '0', '1', '1');
-INSERT INTO `messages` VALUES ('5', '3.5', '0', '3', '1');
-INSERT INTO `messages` VALUES ('6', '1.2.6', '0', '1', '1');
-INSERT INTO `messages` VALUES ('7', '7', '1', '7', '1');
-INSERT INTO `messages` VALUES ('8', '8', '1', '8', '1');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '19');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '20');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '21');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '22');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '23');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '24');
+INSERT INTO `message_contents` VALUES ('test message', 'this is a <strong>very important</strong> test', 'this is a very important test', '25');
 INSERT INTO `messages` VALUES ('9', '9', '1', '9', '1');
 INSERT INTO `messages` VALUES ('10', '10', '1', '10', '3');
-INSERT INTO `messages` VALUES ('12', '3.5.12', '0', '3', '1');
+INSERT INTO `messages` VALUES ('19', '9.19', '0', '9', '1');
+INSERT INTO `messages` VALUES ('20', '9.19.20', '0', '9', '1');
+INSERT INTO `messages` VALUES ('21', '9.21', '0', '9', '1');
+INSERT INTO `messages` VALUES ('22', '9.22', '0', '9', '1');
+INSERT INTO `messages` VALUES ('23', '9.19.20.23', '0', '9', '1');
+INSERT INTO `messages` VALUES ('24', '9.19.24', '0', '9', '1');
+INSERT INTO `messages` VALUES ('25', '9.19.20.23.25', '0', '9', '1');
