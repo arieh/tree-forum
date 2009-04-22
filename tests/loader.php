c8ccfdddae9540f410573954d021fb715077e9c3
arieh.glazer <arieh.glazer@e0e5a594-1eb5-11de-82b5-194b17410922>
2009-04-22 18:48:11 +0000

    loader now outputs valid xhtml

--- tests/loader.php
+++ tests/loader.php
@@ -1,4 +1,11 @@
 <?php
+ob_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<?php
 define('_SEP_',DIRECTORY_SEPARATOR);
 require_once('autoloader.php');
 require_once('..'._SEP_.'classes'._SEP_.'library'._SEP_.'firePHP'._SEP_.'fb.php');
@@ -8,5 +15,13 @@ NewDao::connect('mysql','localhost','root','pass','tree-forum');
 NewDao::setLogger('fb');
 
 $file = $_GET['file'];
-if (strlen($file)>1 && file_exists($file)) include $file;
-else echo "file not found";
+if (strlen($file)>1 && file_exists($file)):
+?>
+<title><?php echo $file;?> :: TF Loader</title></head><body><?php 
+ include $file;
+else: ?>
+<title>Not Found</title></head><body><strong>File Not Found</strong>
+<?php
+endif;?>
+</body></html><?php
+ob_flush();
